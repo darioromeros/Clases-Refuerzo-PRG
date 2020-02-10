07d9b46981857aa0168265fc011f364ec563c712
Programación <Programacin@PC85>
2020-02-10 21:24:26 +0100

    t

--- CrearArraydeTamaño/src/CrearArraydeTamaño.java
+++ CrearArraydeTamaño/src/CrearArraydeTamaño.java
@@ -11,8 +11,22 @@ public class CrearArraydeTamaño {
 		
 		Scanner teclado = new Scanner (System.in); //Creación de un objeto Scanner
 		
+		System.out.println("Introduzca la longitud del array: ");
 		
+		int longitud = teclado.next.Int();
 		
+		int miArray[] = new int[longitud];
+		
+		for (int posicion = 0; posicion < longitud; posicion++) {
+			System.out.println("Introduzca el valor: ");
+			int valor = teclado.nextInt();
+			miArray[posicion] = valor;
+			
+		
+		for (int posicion = 0; posicion < longitud; posicion++) {
+				System.out.println("el valor introducido: " + miArray[posicion]);
+				
+		}
 	}
 
 }
