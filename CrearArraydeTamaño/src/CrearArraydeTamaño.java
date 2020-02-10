ca8d432232c56c589d17224eb1d0ba041c98ed86
Programación <Programacin@PC85>
2020-02-10 21:00:25 +0100

    t

--- CrearArraydeTamaño/src/CrearArraydeTamaño.java
+++ CrearArraydeTamaño/src/CrearArraydeTamaño.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 
 public class CrearArraydeTamaño {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/* 1 Introduce un entero
+		 * 2 Crear un array de ese tamaño
+		 * 3 rellenar el array con valores del usuario.
+		 * 4 Imprimir dichos valores.
+		 */
+		
+		Scanner teclado = new Scanner (System.in); //Creación de un objeto Scanner
+		
+		
+		
 	}
 
 }
