a4c5b4d61a397d560641efb2860e433cbba607c5
Programación <Programacin@PC85>
2020-02-10 20:13:03 +0100

    t

--- Arrays/src/Arrays.java
+++ Arrays/src/Arrays.java
@@ -4,15 +4,20 @@ public class Arrays {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//declaración del array de tamaño
+		//declaración del array de tamaño 3 y tipo entero
 		
 		int miArray[] = new int [3];
 		
-		//asignacion de valor 123 en la 
+		//asignacion de valor 123 en la segunda posicion
 		miArray[1] = 123;
+		miArray[0] = 90;
+		miArray[2] = 20;
 		
 		//referenciar el valor
+		
 		System.out.println("el valor es " + miArray[1]);
+		System.out.println("el valor es " + miArray[0]);
+		System.out.println("el valor es " + miArray[2]);
 	}
 
 }
