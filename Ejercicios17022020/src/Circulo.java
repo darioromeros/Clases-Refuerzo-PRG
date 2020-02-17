8d92f6ac17a61eb0c6396be6720a51c027ef2d41
Programación <Programacin@PC85>
2020-02-17 20:24:30 +0100

    t

--- Ejercicios17022020/src/Circulo.java
+++ Ejercicios17022020/src/Circulo.java
@@ -1,6 +1,14 @@
 
 public class Circulo {
 
+	double radio;
+	String color;
 	
+	public double getArea() {
+		// logica asociada a calcular area
+		double area = Math.PI * radio * radio;
+		return area;
+		
+	}
 	
 }
--- Ejercicios17022020/src/Principal.java
+++ Ejercicios17022020/src/Principal.java
@@ -4,7 +4,32 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//INSTANCIACION
 		
+		Circulo miCirculo = new Circulo();
+		Circulo miOtroCirculo = new Circulo();
+		
+		//ASIGNACION DE VALORES
+		
+		miCirculo.radio = 3;
+		
+		miOtroCirculo.radio = 7;
+		
+		miCirculo.color = "rojo";
+		
+		miOtroCirculo.color = "verde";
+		
+		//REFERENCIAR VALORES
+		
+		System.out.println("Mi primer círculo tiene radio: " + miCirculo.radio);
+		
+		//METODOS
+		
+		double area;
+		
+		area = miCirculo.getArea();
+		
+		System.out.println("El area del primer circulo es: " + area);
 
 	}
 
