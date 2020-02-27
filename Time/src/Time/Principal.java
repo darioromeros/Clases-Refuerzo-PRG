13f48300c0a23147f75b0331fba5c0031bce9019
Programación <Programacin@PC85>
2020-02-27 19:43:05 +0100

    t

---
+++ Time/src/Time/Principal.java
@@ -0,0 +1,18 @@
+package Time;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Time miTiempo = new Time(2, 23, 45);
+		
+		miTiempo.getHoras();
+		
+		System.out.println("horas" + miTiempo.getHoras());
+		
+		miTiempo.setHoras(5);
+		
+		System.out.println("nuevas horas: " + miTiempo.getHoras());
+	}
+
+}
---
+++ Time/src/Time/Time.java
@@ -0,0 +1,23 @@
+package Time;
+
+public class Time {
+	private int horas;
+	private int minutos;
+	private int segundos;
+	
+	public Time(int horas, int minutos, int segundos) {
+	
+		this.horas = horas;
+		this.minutos = minutos;
+		this.segundos = segundos;
+	}
+	//getter
+	public int getHoras() {
+	return this.horas;
+	}
+	
+	//setter
+	public void setHoras(int horas) {
+		this.horas = horas;
+	}
+}
