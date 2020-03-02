b0e009509b0a4cdeeb001e3045f151b1f355b359
Programación <Programacin@PC85>
2020-03-02 19:40:57 +0100

    t

---
+++ date/src/date/Date.java
@@ -0,0 +1,11 @@
+package date;
+
+public class Date {
+	private int ano;
+	private int mes;
+	private int dia;
+	
+	public Date(int ano, int mes, int dia) {
+		this.ano
+	}
+}
---
+++ date/src/date/Principal.java
@@ -0,0 +1,10 @@
+package date;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Date miFecha = new  Date(2, 3, 2020);
+
+	}
+
+}
