15ba92b7a1ca209f0c6b906c2050957095dfaf15
Programación <Programacin@PC85>
2020-03-05 19:55:58 +0100

    t

---
+++ Point/src/Point/Principal.java
@@ -0,0 +1,15 @@
+package Point;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Point pointA = new Point(2,4);
+		Point pointB = new Point(5,1);
+		
+		System.out.println("mi punto A es: " + pointA);
+		System.out.println("mi punto B es: " + pointB);
+			
+
+	}
+
+}
